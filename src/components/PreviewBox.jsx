cf239fd7f054709e90ef10a4be7d2353122be68e
bongie2968 <bongie3598@gmail.com>
2026-03-03 21:52:44 +0900

    Create PreviewBox.jsx

---
+++ src/components/PreviewBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PreviewBox() {
+  return (
+    <div className="box-wrap">
+      <div className="vline left"></div>
+      <div className="vline right"></div>
+      <div className="box169"></div>
+    </div>
+  );
+}
